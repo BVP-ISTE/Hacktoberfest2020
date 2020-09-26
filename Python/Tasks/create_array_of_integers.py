4c37fb503355db54fa80edc8567cc80dbfae627d
Eeshita <43987674+eeshita19@users.noreply.github.com>
2020-09-27 01:41:34 +0530

    Revert "Chaitanya"

--- Python/Tasks/create_array_of_integers.py
+++
@@ -1,9 +0,0 @@
-# Write a Python program to create an array of 5 integers and display the array items.
-#Access individual element through indexes.
-
-from array import *
-
-print("Access first three items individually")
-print(array_num[0])
-print(array_num[1])
-print(array_num[2])
